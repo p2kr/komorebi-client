70c95d9d9920a78d0d11033c28b653576223e0fa
p2kr <34896515+p2kr@users.noreply.github.com>
2026-08-03 20:11:35 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from "@lib/appbar/header.svelte";
+    import Counter from "@lib/Counter.svelte";
+    document.title = "Komorebi";
+</script>
+
+<header>
+    <Header />
+</header>
+<main>
+    <Counter />
+</main>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte(), tailwindcss()],
+  resolve: {
+    tsconfigPaths: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+    },
+  },
+});
